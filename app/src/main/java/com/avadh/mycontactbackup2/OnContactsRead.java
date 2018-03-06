e31aa5e047c0931e362966bf257f8dd8d40b677c
AvadhootPethkar <avadhootpethkar@gmail.com>
2018-03-06 18:16:42 +0530

    third commit

---
+++ app/src/main/java/com/avadh/mycontactbackup2/OnContactsRead.java
@@ -0,0 +1,8 @@
+package com.avadh.mycontactbackup2;
+
+/**
+ * Created by avadh on 3/4/2018.
+ */
+
+public interface OnArrayListFilled {
+}
---
+++ app/src/main/java/com/avadh/mycontactbackup2/RestoreContacts.java
@@ -0,0 +1,8 @@
+package com.avadh.mycontactbackup2;
+
+/**
+ * Created by avadh on 3/6/2018.
+ */
+
+public class RestoreContacts {
+}
